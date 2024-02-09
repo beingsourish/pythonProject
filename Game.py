6cca2c33e09e6c4188b359b97da481aa8d0b21f7
Sourish Banerjee <sourishsmile@gmail.com>
2024-02-09 00:25:26 -0500

    game changes

--- Game.py
+++ Game.py
@@ -2,15 +2,15 @@ import random
 import time
 
 print("Enter your choiGoce ""\n" "1: Sourish ""\n"
-      "2: Ananya ""\n"
-      "3: Katrina ""\n")
+      "2: Salman ""\n"
+      "3: Mahesh babu ""\n")
 a= int(input("Enter here:    "))
 if a==1 :
     print("Your choice is Sourish")
 elif a==2 :
-    print("Your choice is Ananya")
+    print("Your choice is Salman")
 elif a==3 :
-    print("Your choice is Katrina")
+    print("Your choice is Mahesh Babu")
 else:
     print("wrong choice out of Game")
 
@@ -18,8 +18,8 @@ print("let Computer decides................")
 time.sleep(5)
 b= random.randint(1,3)
 if a==1 and b==2 :
-    print("Sourish weds Ananya")
+    print("Sourish looks like  Salman")
 if a==2 and b==1 :
-    print("Sourish weds Ananya")
+    print("Sourish is fan of mahesh babu")
 else:
     print("Sorryyy....")
